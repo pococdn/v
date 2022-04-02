7e6c1fe23e80bbb12dbd87db622625a435e9372e
AnimeGloo <102257120+AnimeGloo@users.noreply.github.com>
2022-04-02 10:19:28 -0500

    Create ads728x90.js

---
+++ ads728x90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '7ed03590776df82b8f31eb29063445a3',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://dictatormiserablealec.com/7ed03590776df82b8f31eb29063445a3/invoke.js"></scr' + 'ipt>');
