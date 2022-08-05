98b94d1a0056d0eb34ac29786ccb0c569b9459a9
AnimeGloo <102257120+AnimeGloo@users.noreply.github.com>
2022-08-05 11:35:02 -0500

    Update episodio2.js

--- animes/0003/episodio2.js
+++ animes/0003/episodio2.js
@@ -1,5 +1,5 @@
 var video = []
-        video[1] = '<iframe src="https://animegloo.github.io/website/player?vid=aHR0cHM6Ly9maWxlcy5jYXRib3gubW9lL3JhcGZzci5tcDQ=" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
+        video[1] = '<iframe src="https://animegloo.github.io/website/player?vid=IQTZsowwKrl_Jipez5ORjrnTdgcg9yoWkpsuu-I7IufT1xf8ogkaOrsgIM7JPqnj" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[2] = '<iframe src="https://www.fembed.com/v/emy31i-mj3-pg8q" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[3] = '<iframe src="https://mega.nz/embed/5lRUUIgJ#OtKgQQBQp7JUT6LqZCAlr7z6i_jCnUJFByMeqWyE818" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[4] = '<iframe src="https://animegloo.github.io/website/player/solid.html?vid=5GcJzvOG7RWEHgaMZqDC0hGXD3M37fjQ4EHfGG9UdKS_w7qvj3gPedjAGBtwBbhD" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
