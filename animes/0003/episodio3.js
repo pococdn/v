3eee7f91e7a9742fbd6ae0ff498e3fbd06590052
AnimeGloo <102257120+AnimeGloo@users.noreply.github.com>
2022-08-06 07:36:59 -0500

    Update episodio3.js

--- animes/0003/episodio3.js
+++ animes/0003/episodio3.js
@@ -2,7 +2,7 @@ var video = []
         video[1] = '<iframe src="https://animegloo.github.io/website/player?vid=IQTZsowwKrl_Jipez5ORjqV5G8EY1F0-mvRG7CGwGuhZlDQ1pOconIfdMt_tGgZd" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[2] = '<iframe src="https://www.fembed.com/v/6z2ykt02ykx6p6n" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[3] = '<iframe src="https://mega.nz/embed/lxJgBKwC#HTJhcUeIwueXx6viRmmU6LHuz86eabEo6jggIgD1uYE" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
-        video[4] = '<iframe src="https://animegloo.github.io/website/player/solid.html?vid=5GcJzvOG7RWEHgaMZqDC0oP9WiDADnQXMZokFgg9TB3ZfLZ4yo1nSvo4mUIfSQ7-" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
+        video[4] = '<iframe src="https://animegloo.github.io/website/player/solid.html?vid=5GcJzvOG7RWEHgaMZqDC0p-1iXq9NP4VE5c7Med54phRoe-VQNVTY9sG01wcxt61" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[5] = '<iframe src="https://animegloo.github.io/website/gdrive.html#MUZreWtLcjJxT3d0bnRHUnNCZ1YyVGtkN1dmZHhLYXpD" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[6] = '<iframe src="https://streamtape.com/e/DqgyGPOG4GHGY3" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         $(document).ready(function(){
