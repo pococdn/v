e301e49725950de63edeb44f3cbf0f724ebcd981
citymovie <102257120+citymovie@users.noreply.github.com>
2022-08-08 08:01:36 -0500

    Update 0001.js

--- peliculas/0001.js
+++ peliculas/0001.js
@@ -2,7 +2,7 @@ var video = []
         video[1] = '<iframe src="https://www.fembed.com/v/emy31i-mp3yqkj4" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[2] = '<iframe src="https://pocoanime.herokuapp.com/drive.php?stream=3zud-r7qsk6PfkzlF8gmD4hS-kxXf9le5DOODZ_t0PGAO_6j9fZ7c1okwWBD0xG83s6dsd8qLLAHkTKz-TmEVE2sirvP4oNfVyni_4fEkso=" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[3] = '<iframe src="https://mega.nz/embed/AoRjBBST#HY5sjI7EPox_eLfdY-vhPud97vOTWXdjkCmCdtelet0" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
-        video[4] = '<iframe src="https://pocoanime.herokuapp.com/encriptar.php?v=V1JPRFNlY0R1MXdOZ0J4cHBDY1JCY3ZmNk00WW4ydzUxRlg3N3gwNk91WllVVVBpT1dNWVR3YzU5cmtpTytPZ2tHMDJkbFd4L3RjSS9JSVFNRVVrVjFwUXgzYXo2d1FaWmpXVnlkRHJPbUxmVzhhT3hPVG1qTTIrS0RpN2FEUGo=" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
+        video[4] = '<iframe src="https://pocoanime.herokuapp.com/encriptar.php?v=QXJCdDJFNUlxUm1ubzQ3V2NHOHZZM2xIV2htenJlTFJNaENldnNCcjRpTE4rQVNNQkUvaEJVUzhleW1ydUh6U2YzQVdPeE1saE9hS1gzekNGV1pHR0E9PQ==" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[5] = '<iframe src="https://sbthe.com/e/gemwf38d8g6p.html" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         video[6] = '<iframe src="https://streamtape.com/e/kb2brl330oIOo0A" frameborder=0 scrolling=no width=100% height=100% allowfullscreen></iframe>';
         $(document).ready(function(){
